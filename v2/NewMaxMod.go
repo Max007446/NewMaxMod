da281ca4ff7096cf77f3f3969f42be08a0430a6e
MaxPsk <max00744@gmail.com>
2021-03-23 22:50:50 +0300

    v2

---
+++ v2/NewMaxMod.go
@@ -0,0 +1,13 @@
+package NewMaxMod
+
+import (
+	_ "fmt"
+
+	_ "github.com/gorilla/websocket"
+	_ "github.com/valyala/fasthttp"
+)
+
+//NewMaxMod return a*b
+func NewMaxMod(a int32, b int32) int32 {
+	return a * b
+}
